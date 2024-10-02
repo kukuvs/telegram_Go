034f4df6909a35cdfcc4fc663ab2830a92fd3288
kukuvs <122643187+kukuvs@users.noreply.github.com>
2024-10-02 21:46:19 +0400

    1

---
+++ src/telegram/telegram.go
@@ -0,0 +1,44 @@
+package telegram
+
+import (
+	"/src/mistral" // Импорт клиента Mistral
+	"log"
+
+	"github.com/mymmrac/telego"
+	"github.com/mymmrac/telego/telegoutil"
+)
+
+// Bot структура для Telegram бота
+type Bot struct {
+	BotClient  *telego.Bot
+	MistralAPI *mistral.MistralClient
+}
+
+// Обработка сообщений
+func (b *Bot) HandleMessages() {
+	updates, err := b.BotClient.UpdatesViaLongPolling(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for update := range updates {
+		if update.Message != nil {
+			userMessage := update.Message.Text
+			response, err := b.MistralAPI.GenerateText(userMessage)
+			if err != nil {
+				log.Printf("Error while sending request to Mistral: %v", err)
+				continue
+			}
+
+			msg := &telego.SendMessageParams{
+				ChatID: telegoutil.ID(update.Message.Chat.ID),
+				Text:   response,
+			}
+
+			_, err = b.BotClient.SendMessage(msg)
+			if err != nil {
+				log.Printf("Error while sending message: %v", err)
+			}
+		}
+	}
+}
